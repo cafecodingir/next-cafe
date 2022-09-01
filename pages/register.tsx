fab6afed53ad11772ef8dbc0cc6b1c257562c8e0
Mohsen Nadalinezhad <cafecodingir@gmail.com>
2022-09-02 03:13:21 +0430

    master page

---
+++ pages/register.tsx
@@ -0,0 +1,12 @@
+import Master from "./master";
+import styles from '../styles/Home.module.css'
+
+export default function Register() {
+  return (
+    <Master>
+      <div className={styles.container}>
+        <main className={styles.main}>عضویت</main>
+      </div>
+    </Master>
+  )
+}
